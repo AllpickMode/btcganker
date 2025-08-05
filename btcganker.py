a5d81d8944b4f787628a6234f283420eea59508d
AllpickMode <88521113@qq.com>
2025-08-05 16:46:41 +0800

    优化

--- btcganker.py
+++ btcganker.py
@@ -1,123 +1,64 @@
 import addresses
 import multiprocessing
-import smtplib
-from email.mime.text import MIMEText
-from email.header import Header
-import time
 import sys
 import os
+from concurrent.futures import ProcessPoolExecutor
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_dir, 'lib')) 
 import bitcoin
-import config
 
-CPU_HALF = config.CPU_HALF
-MIN_WORKERS = config.MIN_WORKERS
-ENABLE_INTERNET = config.ENABLE_INTERNET
+CPU_HALF = False
+MIN_WORKERS = 1
 
-def generate_key():
+def generate_key_pair():
     try:
         private_key = bitcoin.random_key()
-        return private_key
-    except Exception as e:
-        print('Error')
-        return None
-
-def generate_public_key(private_key):
-    try:
         public_key = bitcoin.privtopub(private_key)
-        return public_key
-    except Exception as e:
-        print('Error')
-        return None
-
-def generate_address(public_key):
-    try:
         address = bitcoin.pubtoaddr(public_key)
-        return address
+        # print(f'Generating key: {private_key}-{public_key}-{address}')
+        return private_key, public_key, address
     except Exception as e:
-        print('Error')
-        return None
+        print(f'Error generating key pair: {e}')
+        return None, None, None
 
-def compare_address(address):
-    return address in addresses.TARGET_ADDRESSES
+def check_and_save(key_pair):
+    private_key, public_key, address = key_pair
+    if address in addresses.TARGET_ADDRESSES:
+        save_key(private_key, public_key, address)
+    return False
 
 def save_key(private_key, public_key, address):
     try:
         with open('btcganker.txt', 'a') as f:
-            f.write(private_key)
-            f.write('-')
-            f.write(public_key)
-            f.write('-')
-            f.write(address)
-            f.write('-')
+            f.write(f"{private_key}-{public_key}-{address}\n")
     except Exception as e:
-        print('Ok')
+        print(f'Error saving key: {e}')
 
-def send_mail(private_key, public_key, address):
-    print('Send mail')
-
-    smtp_server = config.SMTP_SERVER
-    smtp_port = config.SMTP_PORT                
-    sender_email = config.SENDER_EMAIL
-    receiver_email = config.RECEIVER_EMAIL
-    password = config.SENDER_PASSWORD
-
-    message = MIMEText(private_key+'-'+public_key+'-'+address, 'plain', 'utf-8') 
-    message['From'] = Header('<%s>' % sender_email, 'utf-8')
-    message['To'] = Header('<%s>' % receiver_email, 'utf-8')
-    message['Subject'] = Header('Send mail', 'utf-8')
-
-    try:
-        with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, [receiver_email], message.as_string())
-        print('Ok')
-    except Exception as e:
-        print('Error')
-        return None
+def worker():
+    while True:
+        key_pair = generate_key_pair()
+        if key_pair[0]:
+            if check_and_save(key_pair):
+                break
 
 def main():
-    starTIME=time.time()
-    i = 0
-    emailFlag = True
-    while True:
-        nowTIME=time.time()
-        if emailFlag:
-            i += 1
-            if nowTIME-starTIME > 10:
-                emailFlag = False
-                print('Now time-' +  str(nowTIME))
-                print('Already running-' + str(i))
-                if ENABLE_INTERNET:
-                    send_mail('10 mins','-',str(i))
-        private_key = generate_key()
-        public_key = generate_public_key(private_key)
-        address = generate_address(public_key)
-        if compare_address(address):
-            save_key(private_key, public_key, address)
-            if ENABLE_INTERNET:
-                send_mail(private_key, public_key, address)
-            break
+    print('Starting Bitcoin address generator')
+    
+    num_cores = multiprocessing.cpu_count()
+    if CPU_HALF:
+        num_workers = max(int(num_cores / 2), MIN_WORKERS)
+        print('Running in low power mode')
+    else:
+        num_workers = max(num_cores - 1, MIN_WORKERS)
+        print('Running in high performance mode')
+    
+    print(f'Using {num_workers} worker processes')
+    print(f'Target addresses: {len(addresses.TARGET_ADDRESSES)}')
+    
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        futures = [executor.submit(worker) for _ in range(num_workers)]
+        for future in futures:
+            future.result()
 
 if __name__ == '__main__':
-    num = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool()
-    if CPU_HALF:
-        num = int(num/2)
-        print('Low model')
-    else:
-        num = num - 1
-        print('High model')
-    if num < MIN_WORKERS:
-        num = MIN_WORKERS
-        print('Min model')
-    print('Core num - ' + str(num))
-    print('Addrs - ' + str(len(addresses.TARGET_ADDRESSES)))
-    print('Start time - ' +  str(time.time()))
-    for i in range(num):
-        pool.apply_async(main)
-    pool.close()
-    pool.join()
-
-
+    main()
